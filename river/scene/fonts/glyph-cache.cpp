317d144a13f5c7c65198649551c857fb5ce2c095
Zoxc <zoxc32@gmail.com>
2011-02-21 03:30:58 +0100

    Cleanup.

--- river/scene/fonts/glyph-cache.cpp
+++ river/scene/fonts/glyph-cache.cpp
@@ -11,7 +11,7 @@ namespace River
 
 		glBindTexture(GL_TEXTURE_2D, texture);
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 		
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
